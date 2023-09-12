73a3f288af3d346181bf16d02bee56f58008098f
AlexeyYalovoy <ledinger2015@gmail.com>
2023-09-12 09:39:09 +0200

    new

---
+++ src/main/java/com/example/TestAston/TestAstonApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestAston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAstonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAstonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestAston/TestAstonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestAston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAstonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
